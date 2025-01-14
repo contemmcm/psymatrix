c1277a1240bbe7fc3e536547683d92599b0cc78c
Marcio Monteiro <muriloconte@gmail.com>
2025-01-14 22:35:05 +0100

    fix: wip

--- psymatrix/finetune/progressive.py
+++ psymatrix/finetune/progressive.py
@@ -261,11 +261,15 @@ def run():
         models_ids = [args.model_id]
 
     for model_id in models_ids:
-        ftuner = ProgressiveFineTuning(
-            model_id=model_id,
-            dataset_name_or_path=args.dataset_id,
-            hyperparameters=hyperparameters,
-        )
+        try:
+            ftuner = ProgressiveFineTuning(
+                model_id=model_id,
+                dataset_name_or_path=args.dataset_id,
+                hyperparameters=hyperparameters,
+            )
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
 
         if ftuner.is_output_file_present():
             print(f"Skipping {model_id}...")
